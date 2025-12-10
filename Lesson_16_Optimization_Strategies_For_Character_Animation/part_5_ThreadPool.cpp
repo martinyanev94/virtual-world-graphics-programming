06312917cfb57518d7053e3f1bccf01fb3a0b0cf
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-10 01:55:28 -0500

    Add Lesson_16_Optimization_Strategies_For_Character_Animation/part_5_ThreadPool.cpp

---
+++ Lesson_16_Optimization_Strategies_For_Character_Animation/part_5_ThreadPool.cpp
@@ -0,0 +1,60 @@
+#include <thread>
+#include <vector>
+#include <functional>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads) {
+        for (size_t i = 0; i < numThreads; ++i) {
+            workers.emplace_back([this] {
+                while (true) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(this->queueMutex);
+                        this->condition.wait(lock, [this] {
+                            return this->stop || !this->tasks.empty();
+                        });
+
+                        if (this->stop && this->tasks.empty())
+                            return;
+
+                        task = std::move(this->tasks.front());
+                        this->tasks.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    template<class F>
+    void enqueue(F&& f) {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            tasks.emplace(std::forward<F>(f));
+        }
+        condition.notify_one();
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            stop = true;
+        }
+        condition.notify_all();
+        for (std::thread& worker : workers) {
+            worker.join();
+        }
+    }
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queueMutex;
+    std::condition_variable condition;
+    bool stop = false;
+};
+
+// Usage
+ThreadPool pool(std::thread::hardware_concurrency());
+pool.enqueue([this]() { this->bones[i]->getFinalTransform(); });
