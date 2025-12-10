60a05041cd1b714f1f55511e4e2bfa5d0dae0f6e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-10 01:57:03 -0500

    Add Lesson_28_Performance_Optimization_Strategies/part_4_vertex_buffer_setup.cpp

---
+++ Lesson_28_Performance_Optimization_Strategies/part_4_vertex_buffer_setup.cpp
@@ -0,0 +1,11 @@
+GLuint characterVBO;
+glGenBuffers(1, &characterVBO);
+glBindBuffer(GL_ARRAY_BUFFER, characterVBO);
+glBufferData(GL_ARRAY_BUFFER, characterVertices.size() * sizeof(Vertex), characterVertices.data(), GL_DYNAMIC_DRAW);
+glBindBuffer(GL_ARRAY_BUFFER, characterVBO);
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+glEnableVertexAttribArray(0);
+glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+glEnableVertexAttribArray(1);
+glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoord));
+glEnableVertexAttribArray(2);
