07be7bae9960197b54dddaad7af842a06cfe207f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-10 01:55:48 -0500

    Add Lesson_19_Updating_Character_Data/part_2_CharacterData.cpp

---
+++ Lesson_19_Updating_Character_Data/part_2_CharacterData.cpp
@@ -0,0 +1,6 @@
+struct CharacterData {
+    glm::vec3 position;  // Position in 3D space
+    glm::vec3 rotation;  // Rotation angles
+    glm::vec3 scale;     // Scaling factors
+    int animationState;  // Current animation state
+};
